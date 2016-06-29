67a98f47e0400acf3ac786e45c8b96f69a52c276
Joel Waage and Rouz Majlessi <joel+rouz@>
2016-06-29 16:18:08 -0700

    clear result array and clear display when hold button is pressed

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 // business logic
 var dieRoll = '';
 var result = [];
-var total = 0
+var result2 = [];
+var totalPlayer1 = 0
+var totalPlayer2 = 0
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// function Player(playOrder, result, total) {
+//   this.order = playOrder;
+//   this.result = result;
+//   this.total = total;
+// }
+//
+// Player.prototype.rollDie = function() {
+//   console.log("huh");
+// }
 
 
 
@@ -18,9 +29,34 @@ $(document).ready(function() {
     $("#resultPlayer1").append("<li>" + dieRoll + "</li>")
   });
 
-  $("form#holdPlayer1").click(function(event) {
+  $("form#player2").click(function(event) {
     event.preventDefault();
+    dieRoll = getRandomInt(1,7);
+    result2.push(dieRoll);
+    $("#resultPlayer2").append("<li>" + dieRoll + "</li>")
+  });
+
+  $("form#holdPlayer1").click(function(event) {
+    $("#resultPlayer1").empty();
+    event.preventDefault();
+    for (var i=0; i < result.length; i++) {
+     totalPlayer1 += result[i];
+    }
+    result = [];
+    $(".totalPlayer1").text(totalPlayer1);
+  });
+
+  $("form#holdPlayer2").click(function(event) {
+    $("#resultPlayer2").empty();
+    event.preventDefault();
+    for (var i=0; i < result2.length; i++) {
+    totalPlayer2 += result2[i];
+    }
+    result2 = [];
+      $(".totalPlayer2").text(totalPlayer2);
 
   });
+
+
 
 });

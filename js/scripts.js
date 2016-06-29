5cca3248ed6fca63d577c536f365ee147b644f01
Joel Waage and Rouz Majlessi <joel+rouz@>
2016-06-29 10:55:45 -0700

    add button and functionality to generate an array with results between 1 and 6

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
+// business logic
+var dieRoll = '';
+var result = [];
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 
 
 
-
-
-
-
-
+// user logic
 $(document).ready(function() {
-
+  $("form#player1").click(function(event) {
+    event.preventDefault();
+    dieRoll = getRandomInt(1,7);
+    result.push(dieRoll);
+    console.log(result);
+  });
 });

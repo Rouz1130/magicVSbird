78f02c8d660b2527fdaf7250b3c9913d172ee7f3
Joel Waage and Rouz Majlessi <joel+rouz@>
2016-06-29 17:23:03 -0700

    add graphics and functionality for busting and winning

--- js/scripts.js
+++ js/scripts.js
@@ -8,32 +8,35 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// function Player(playOrder, result, total) {
-//   this.order = playOrder;
-//   this.result = result;
-//   this.total = total;
-// }
-//
-// Player.prototype.rollDie = function() {
-//   console.log("huh");
-// }
-
-
 
 // user logic
 $(document).ready(function() {
   $("form#player1").click(function(event) {
     event.preventDefault();
+    $("#bust2").hide();
     dieRoll = getRandomInt(1,7);
-    result.push(dieRoll);
-    $("#resultPlayer1").append("<li>" + dieRoll + "</li>")
+    if(dieRoll === 1) {
+      $("#bust1").show();
+      $("#resultPlayer1").empty();
+      return result = [];
+    } else {
+      result.push(dieRoll);
+      $("#resultPlayer1").append("<li>" + dieRoll + "</li>")
+    }
   });
 
   $("form#player2").click(function(event) {
     event.preventDefault();
+    $("#bust1").hide();
     dieRoll = getRandomInt(1,7);
+    if(dieRoll === 1) {
+      $("#bust2").show();
+      $("#resultPlayer2").empty();
+      return result2 = [];
+    } else {
     result2.push(dieRoll);
     $("#resultPlayer2").append("<li>" + dieRoll + "</li>")
+  }
   });
 
   $("form#holdPlayer1").click(function(event) {
@@ -41,9 +44,13 @@ $(document).ready(function() {
     event.preventDefault();
     for (var i=0; i < result.length; i++) {
      totalPlayer1 += result[i];
+   } if (totalPlayer1 >= 100) {
+      $(".totalPlayer1").text(totalPlayer1);
+      $("#win").show();
+    } else {
+      result = [];
+      $(".totalPlayer1").text(totalPlayer1);
     }
-    result = [];
-    $(".totalPlayer1").text(totalPlayer1);
   });
 
   $("form#holdPlayer2").click(function(event) {
@@ -51,10 +58,13 @@ $(document).ready(function() {
     event.preventDefault();
     for (var i=0; i < result2.length; i++) {
     totalPlayer2 += result2[i];
-    }
+  } if (totalPlayer2 >= 100) {
+    $(".totalPlayer2").text(totalPlayer2);
+    $("#win").show();
+  } else {
     result2 = [];
-      $(".totalPlayer2").text(totalPlayer2);
-
+    $(".totalPlayer2").text(totalPlayer2);
+  }
   });
 
 
